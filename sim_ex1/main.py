48d2eec81b6e10be3f42b3e59ffffd1896085b67
Joven Nepomuceno <14026114+jrnepomuceno@users.noreply.github.com>
2025-09-18 23:57:00 +0800

    Latest revision

--- sim_ex1/main.py
+++ sim_ex1/main.py
@@ -1,51 +1,91 @@
 import random
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 import sys
+
+def show_plot(x_range, data, p, name, x_label="Successes", y_label="", figure_num=1) -> None:
+
+    plt.figure(figure_num)
+    plt.bar(x_range, data)
+    plt.xlabel(f"Number of {x_label}")
+    plt.ylabel("Probability")
+    plt.title(f"Probability Distribution of {name} p={p}")
+
+def generate_biased_bitstream(length, p) -> str:
+    if not (0 <= p <= 1):
+        raise ValueError("Probability p must be between 0 and 1.")
+    bitstream = map(lambda x: "1" if x > p else "0", [random.randint(0,1) for _ in range(length)])
+    return "".join(bitstream)
+
+def combination(n, r) -> float:
+    return (math.factorial(n)) // (math.factorial(r) * math.factorial(n - r))
+
+"""
+PART 1
+"""
+def binomial_sim_rv(n, p, num_samples) -> list[int]:
+    samples = [generate_biased_bitstream(n, p).count("1") for _ in range(0,num_samples)]
+    return samples
+
+def neg_binomial_sim_rv(k, n, p, num_samples) -> list[int]:
+    samples = []
+    for _ in range(0,num_samples):
+        bits = generate_biased_bitstream(n, p)
+        for i in range(1, n):
+            if bits[:i].count("1") == k:
+                samples.append(i)
+                break
+
+    return samples
+
+def binomial_theo_rv(n, p=0.) -> list[int]:
+
+    x_range = np.arange(0, n)
+    result = [combination(n, x) * ((float(p) ** x) * ((1 - float(p)) ** (n - x))) for x in x_range]
+
+    show_plot(x_range, result, p, name=f"Binomial RV: n={n}", x_label="Successes", figure_num=1)
+
+    return result
+
+def neg_binomial_theo_rv(k, p=0., num_trials=0):
+
+    x_range = range(k, num_trials)
+    result = [combination(x-1, k-1) * ((p ** k) * (1 - p) ** (x - k)) for x in x_range]
+    show_plot(x_range, result, p, name=f"Negative Binomial RV: k={k}", x_label="Trials", figure_num=2)
+    return result
+
+
+"""
+PART 2
+"""
+
+def exponential_sim_rv(rate, num_samples) -> list[np.float16]:
+    uni_stream = [round(random.uniform(0, 1), 5) for _ in range(0, num_samples)]
+    exp_cdf_inverse_func = lambda y: round(-1*(np.log(1 - y)) / rate, 2)
+    samples = list(map(exp_cdf_inverse_func, uni_stream))
+    return samples
 
 def main():
 
     n = 100
-    # Generate random binary stream
-    rand_bin = str(bin(random.randint(0, (2 ** n))))[2:]
-    # pad zeroes at the start of string if needed until length n
-    if len(rand_bin) < n:
-        rand_bin = rand_bin.zfill(n)
+    p = 0.3
 
-    # data = binomial_rv(rand_bin, n)
-    data = neg_binomial_rv(rand_bin, 34)
+    # data_1 = binomial_sim_rv(n, p, 1000)
+    # plt.figure(1)
+    # plt.hist(data_1, bins=n)
 
+    # data_2 = neg_binomial_sim_rv(34, n, p, 1000)
+    # # plt.figure(2)
+    # plt.hist(data_2, bins=n)
 
-def combination(n, r):
-    return (math.factorial(n)) // (math.factorial(r) * math.factorial(n - r))
+    data_3 = exponential_sim_rv(0.5, 500)
+    plt.hist(data_3, bins=n)
 
-def binomial_rv(rand_bin, n):
-    p = round(list(rand_bin).count('1') / n, 2)
-    result = [combination(n, x) * (p ** x) * ((1 - p) ** (n - x)) for x in range(0, n)]
+    # b_data, b_p = binomial_theo_rv(n, p, rand_bin=rand_bin)
+    # negb_data, negb_p = neg_binomial_theo_rv(34, p, num_trials=100, rand_bin=rand_bin)
 
-    plt.hist(result, bins=20)
-    plt.xlabel("Value")
-    plt.ylabel("Frequency")
-    plt.title(f"Distribution of Data: p={p} n={n}")
     plt.show()
-
-    return result
-
-def neg_binomial_rv(rand_bin, k):
-    
-    rand_len = len(rand_bin)
-    for index, item in enumerate(rand_bin):
-        if item == '1': 
-            k -= 1
-            if k == 0: 
-                rand_bin = rand_bin[:index+1]
-                break
-
-    if len(rand_bin) < rand_len:
-        rand_bin = rand_bin.zfill(rand_len)
-
-    # print(rand_bin, len(rand_bin), rand_bin.count('1'))
-    result = [combination(x-1, k-1) * (p ** k) * ((1 - p) ** (x - k)) for x in range(0, rand_len)]
 
 if __name__ == "__main__":
     main()
